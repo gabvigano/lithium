af7a0d8f9d1f8f2af38981a4ab9470bc03b81661
git <gab.vigano@gmail.com>
2025-07-24 19:50:41 +0200

    support for show/hide objects

--- lithium-engine/src/mq_adapter.rs
+++ lithium-engine/src/mq_adapter.rs
@@ -16,22 +16,24 @@ pub fn color_to_mq(color: &components::Color) -> prelude::Color {
 }
 
 pub fn render(world: &world::World, entity: entities::Entity, camera: &scene::Camera) {
-    let components::Pos { x, y } = world.pos.get(entity).expect("missing position");
-    let components::Pos { x: cam_x, y: cam_y } = camera.pos();
+    if *world.show.get(entity).expect("missing show") {
+        let components::Pos { x, y } = world.pos.get(entity).expect("missing position");
+        let components::Pos { x: cam_x, y: cam_y } = camera.pos();
 
-    match world.shape.get(entity).expect("missing shape") {
-        components::Shape::Rect(rect) => prelude::draw_rectangle(
-            x - cam_x,
-            y - cam_y,
-            rect.width,
-            rect.height,
-            color_to_mq(world.color.get(entity).expect("missing color")),
-        ),
-        components::Shape::Circle(circle) => prelude::draw_circle(
-            x + circle.radius - cam_x, // sum radius because macroquad use centre for circles instead of top left
-            y + circle.radius - cam_y,
-            circle.radius,
-            color_to_mq(world.color.get(entity).expect("missing color")),
-        ),
+        match world.shape.get(entity).expect("missing shape") {
+            components::Shape::Rect(rect) => prelude::draw_rectangle(
+                x - cam_x,
+                y - cam_y,
+                rect.width,
+                rect.height,
+                color_to_mq(world.color.get(entity).expect("missing color")),
+            ),
+            components::Shape::Circle(circle) => prelude::draw_circle(
+                x + circle.radius - cam_x, // sum radius because macroquad use centre for circles instead of top left
+                y + circle.radius - cam_y,
+                circle.radius,
+                color_to_mq(world.color.get(entity).expect("missing color")),
+            ),
+        }
     }
 }
